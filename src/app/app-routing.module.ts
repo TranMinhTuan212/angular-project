6bba9c4635a08e11252637aae309e55afe18d55a
Trần Minh Tuấn <tuan9a5nbk@gmail.com>
2023-04-30 11:33:15 +0700

    update full layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WomenComponent } from './pages/women/women.component';
+import { MenComponent } from './pages/men/men.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ChildrenComponent } from './pages/children/children.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'men', component: MenComponent},
+  {path: 'women', component: WomenComponent},
+  {path: 'children', component: ChildrenComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BackgroundComponent } from './components/background/background.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WomenComponent } from './pages/women/women.component';
+import { MenComponent } from './pages/men/men.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FormLoginComponent } from './components/form-login/form-login.component';
+import { FormRegisterComponent } from './components/form-register/form-register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ChildrenComponent } from './pages/children/children.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BackgroundComponent,
+    MainComponent,
+    FooterComponent,
+    WomenComponent,
+    MenComponent,
+    ContactComponent,
+    FormLoginComponent,
+    FormRegisterComponent,
+    HomeComponent,
+    RegisterComponent,
+    ChildrenComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
