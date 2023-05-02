02ef32feb3dce71a0f36857d7b6b8809bce6f8d4
Trần Minh Tuấn <tuan9a5nbk@gmail.com>
2023-05-02 11:03:00 +0700

    update slider

--- src/app/components/background/background.component.ts
+++ src/app/components/background/background.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -17,6 +17,32 @@ export class BackgroundComponent {
   paused = false;
   unpauseOnArrow = false;
   pauseOnIndicator = false;
+	pauseOnHover = true;
+	pauseOnFocus = true;
 
   @ViewChild('carousel', { static: true }) carousel!: NgbCarousel;
+
+
+
+	togglePaused() {
+		if (this.paused) {
+			this.carousel.cycle();
+		} else {
+			this.carousel.pause();
+		}
+		this.paused = !this.paused;
+	}
+
+	onSlide(slideEvent: NgbSlideEvent) {
+		if (
+			this.unpauseOnArrow &&
+			slideEvent.paused &&
+			(slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+		) {
+			this.togglePaused();
+		}
+		if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+			this.togglePaused();
+		}
+	}
 }
