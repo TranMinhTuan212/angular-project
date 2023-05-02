775dcba2748d24fafa4826786fcbe82c0c4b583e
Trần Minh Tuấn <tuan9a5nbk@gmail.com>
2023-05-02 16:24:15 +0700

    update contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  public messages: Array<string> = [];
+
+  @ViewChild('contact') contact!: ElementRef;
+  @ViewChild('keyPress') keyPress!: ElementRef;
+
+  constructor(private render: ChangeDetectorRef ){}
+
+  addMessage(inputName: HTMLInputElement){
+    if(inputName.value.trim().length > 0){
+      this.messages.push(inputName.value)
+      inputName.value = ''
+      inputName.focus()
+      this.render.detectChanges()
+    }
+  }
+
+  toggleMessage(){
+    this.contact.nativeElement.classList.toggle('d-none')
+  }
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
+  
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.addMessage(this.keyPress.nativeElement);
+    }
+  }
 }
